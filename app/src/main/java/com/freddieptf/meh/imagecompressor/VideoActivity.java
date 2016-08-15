b2c3fbed1fd74841d8a8e83499c2554b776a8db8
freddieptf <freddmuiru@gmail.com>
2016-08-15 20:00:24 +0300

    add bottom sheet with progress details for running tasks

--- app/src/main/java/com/freddieptf/meh/imagecompressor/VideoActivity.java
+++ app/src/main/java/com/freddieptf/meh/imagecompressor/VideoActivity.java
@@ -13,20 +13,23 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
@@ -45,6 +48,7 @@ import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -55,23 +59,29 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
     boolean ffmpegEnabled = false;
     String[] videoDetails;
     String[] presets;
-    String threads;
+    String[] threads;
     Bitmap t;
     public static final  String VID_DETS = "vid_dets";
     private static final String TAG      = "CompressVidActivity";
 
     GetVidDetailsFromUri getVidDetails;
+    TasksStatusAdapter tasksStatusAdapter;
+    ArrayList<String> tasksStatusStringArray;
 
     Spinner spinnerThreads, spinnerPresets, spinnerContainers;
     EditResolutionView resolutionView;
     ImageView ivThumbnail;
-    TextView tvVideoDuration, tvVideoResolution, tvVideoSize, tvVideoName, tvVideoQuality;
+    TextView tvVideoDuration, tvVideoResolution, tvVideoSize, tvVideoName, tvVideoQuality, tvTaskStatus;
     SeekBar sbVideoQuality;
     RadioGroup radioGroup;
     FloatingActionButton fab;
     TaskView taskViewScale, taskViewConvert;
     Toolbar toolbar;
     LinearLayout taskParent;
+    CoordinatorLayout coordinatorLayout;
+    View bottomSheet;
+    BottomSheetBehavior bottomSheetBehavior;
+    RecyclerView taskStatusRecycler;
 
     public static final int KEY_PATH       = 0;
     public static final int KEY_TITLE      = 1;
@@ -88,6 +98,11 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
 
         toolbar           = (Toolbar) findViewById(R.id.toolbar);
         taskParent        = (LinearLayout) findViewById(R.id.parent);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.content);
+
+        bottomSheet       = coordinatorLayout.findViewById(R.id.bottomSheet);
+        taskStatusRecycler= (RecyclerView) findViewById(R.id.recyclerTaskStatus);
+        tvTaskStatus      = (TextView) findViewById(R.id.tv_taskStatus);
 
         tvVideoName       = (TextView)  findViewById(R.id.tv_videoName);
         tvVideoDuration   = (TextView)  findViewById(R.id.tv_videoDuration);
@@ -107,29 +122,17 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
         sbVideoQuality    = (SeekBar)  findViewById(R.id.sk_vidQuality);
         radioGroup        = (RadioGroup) findViewById(R.id.rgButtons);
 
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey("ff"))
-                ffmpegEnabled = savedInstanceState.getBoolean("ff");
-            else initFfmpeg();
+        restoreState(savedInstanceState);
+        if (getVidDetails != null) getVidDetails.execute();
 
-            if (savedInstanceState.containsKey(VID_DETS)) {
-                videoDetails = savedInstanceState.getStringArray(VID_DETS);
-                initVideoDetailView(videoDetails);
-            }
-            else {
-                getVidDetails = new GetVidDetailsFromUri(Uri.parse(getIntent().getStringExtra(CameraActionHandlerService.MEDIA_URI)));
-            }
+        ViewGroup.LayoutParams params = bottomSheet.getLayoutParams();
+        params.height = getResources().getDisplayMetrics().heightPixels
+                - getResources().getDimensionPixelSize(R.dimen.expanded_toolbar)
+                - getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin);
+        bottomSheet.setLayoutParams(params);
 
-            if(savedInstanceState.containsKey("presets")) presets = savedInstanceState.getStringArray("presets");
-            resolutionView.restoreState(savedInstanceState);
-        } else {
-            initFfmpeg();
-            getVidDetails = new GetVidDetailsFromUri(Uri.parse(getIntent().getStringExtra(CameraActionHandlerService.MEDIA_URI)));
-        }
-
-        if (getVidDetails != null){
-            getVidDetails.execute();
-        }
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
 
         taskViewScale.setOnTaskCheckedListener(this);
         taskViewConvert.setOnTaskCheckedListener(this);
@@ -143,48 +146,66 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
 
     }
 
+    private void restoreState(@Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("ff"))
+                ffmpegEnabled = savedInstanceState.getBoolean("ff");
+            else initFfmpeg();
+
+            if(savedInstanceState.containsKey("presets")) presets = savedInstanceState.getStringArray("presets");
+            if(savedInstanceState.containsKey("threads")) threads = savedInstanceState.getStringArray("threads");
+
+            if (savedInstanceState.containsKey(VID_DETS)) {
+                videoDetails = savedInstanceState.getStringArray(VID_DETS);
+                initVideoDetailView(videoDetails);
+            }
+            else {
+                getVidDetails = new GetVidDetailsFromUri(Uri.parse(getIntent().getStringExtra(CameraActionHandlerService.MEDIA_URI)));
+            }
+
+            if(tasksStatusAdapter != null) tasksStatusAdapter.restoreState(savedInstanceState);
+            resolutionView.restoreState(savedInstanceState);
+        } else {
+            initFfmpeg();
+            getVidDetails = new GetVidDetailsFromUri(Uri.parse(getIntent().getStringExtra(CameraActionHandlerService.MEDIA_URI)));
+        }
+    }
+
     public void onFabClick(View view){
-        if(((CheckBox) taskViewScale.findViewById(R.id.taskCheck)).isChecked()){
+        if(taskViewScale.isChecked()){
             CompressUtils.scaleVideo(this,
                     videoDetails[VideoActivity.KEY_PATH],
                     new int[]{resolutionView.getResWidth(), resolutionView.getResHeight()},
-                    threads
+                    spinnerThreads.getSelectedItem().toString()
             );
-        }else if (((CheckBox) taskViewConvert.findViewById(R.id.taskCheck)).isChecked()){
+            hideFab();
+        }else if (taskViewConvert.isChecked()){
             String container = spinnerContainers.getSelectedItem().toString();
             String crf = String.valueOf(sbVideoQuality.getProgress() + 18); //plus 18 cause we're faking the start (zero) as 18
             String encodingPreset = CompressUtils.getEncodingPreset(radioGroup.getCheckedRadioButtonId());
             CompressUtils.convertVideo(this, videoDetails[VideoActivity.KEY_PATH], container, crf, encodingPreset);
+            hideFab();
         }else {
             Snackbar.make(view, "You need to select atleast one Task", Snackbar.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    public void onTaskCheck(boolean b, TaskView taskView) {
-        switchTasks(b, taskView);
+    public void onTaskCheck(String tag) {
+//        Log.d(TAG, "onTaskCheck: " + taskView.getTag() + "/" + b);
 //        if(b) taskView.findViewById(R.id.taskBody).setVisibility(View.VISIBLE);
 //        else taskView.findViewById(R.id.taskBody).setVisibility(View.GONE);
+        switchTasks(tag);
     }
 
-    //placeholder until i enable task linking where one tasks output is another tasks input
-    private void switchTasks(boolean b, TaskView taskView){
-        if(taskView.getTag().equals("scale")){
-            if(b) {
-                taskViewScale.findViewById(R.id.taskBody).setVisibility(View.VISIBLE);
-                taskViewConvert.findViewById(R.id.taskBody).setVisibility(View.GONE);
-                ((CheckBox) taskViewConvert.findViewById(R.id.taskCheck)).setChecked(false);
-            } else {
-                taskViewScale.findViewById(R.id.taskBody).setVisibility(View.GONE);
-            }
-        }else {
-            if(b) {
-                taskViewConvert.findViewById(R.id.taskBody).setVisibility(View.VISIBLE);
-                taskViewScale.findViewById(R.id.taskBody).setVisibility(View.GONE);
-                ((CheckBox) taskViewScale.findViewById(R.id.taskCheck)).setChecked(false);
-            }else {
-                taskViewConvert.findViewById(R.id.taskBody).setVisibility(View.GONE);
-            }
+    public void switchTasks(String tag){
+        Log.d(TAG, taskViewScale.isChecked() + "/" + taskViewConvert.isChecked());
+        if(tag.equals("scale")) {
+            taskViewScale.toggle();
+            if(taskViewConvert.isChecked()) taskViewConvert.setChecked(false);
+        }else if(tag.equals("convert")){
+            taskViewConvert.toggle();
+            if(taskViewScale.isChecked()) taskViewScale.setChecked(false);
         }
     }
 
@@ -192,9 +213,10 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean("ff", ffmpegEnabled);
-        if (videoDetails != null && videoDetails.length > 0) outState.putStringArray(VID_DETS, videoDetails);
+        if(videoDetails != null && videoDetails.length > 0) outState.putStringArray(VID_DETS, videoDetails);
         if(presets != null) outState.putStringArray("presets", presets);
-        if(threads != null && !threads.isEmpty()) outState.putString("threads", threads);
+        if(threads != null) outState.putStringArray("threads", threads);
+        if(tasksStatusAdapter != null) tasksStatusAdapter.saveState(outState);
         resolutionView.saveState(outState);
     }
 
@@ -314,6 +336,8 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
         tvVideoDuration.setText(String.valueOf(TimeUnit.MILLISECONDS.toSeconds(Long.parseLong(videoDetails[KEY_DURATION]))) + "seconds");
         tvVideoResolution.setText(videoDetails[KEY_RESOLUTION]);
         tvVideoSize.setText(Long.parseLong(videoDetails[KEY_SIZE]) / 1024000 + "mb");
+        initTaskScale();
+        initTaskConvert();
     }
 
     private void animateToolBar(){
@@ -322,7 +346,7 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
         int toolBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, getResources().getDisplayMetrics());
 
         ValueAnimator heightAnimator = ValueAnimator.ofInt(toolBarHeight, getResources().getDimensionPixelSize(R.dimen.expanded_toolbar));
-        heightAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        heightAnimator.setInterpolator(new DecelerateInterpolator(1.2f));
         heightAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
@@ -367,6 +391,10 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
         heightAnimator.start();
     }
 
+    private void hideFab(){
+       fab.setVisibility(View.GONE);
+    }
+
     private void initTaskScale(){
         String[] res = videoDetails[VideoActivity.KEY_RESOLUTION].split("x");
         int w = Integer.parseInt(res[0]);
@@ -378,24 +406,12 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
         }
         resolutionView.setResolution(w, h);
 
+        if(threads == null) threads = new String[]{Runtime.getRuntime().availableProcessors()-1 + " ", Runtime.getRuntime().availableProcessors() + " " };
         ArrayAdapter<String> threadSpinnerAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, android.R.id.text1,
-                new String[]{Runtime.getRuntime().availableProcessors()-1 + " ", Runtime.getRuntime().availableProcessors() + " " });
+                threads);
         threadSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerThreads.setAdapter(threadSpinnerAdapter);
-        spinnerThreads.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position > 0){
-                    threads = ((TextView) view.findViewById(android.R.id.text1)).getText().toString();
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         ArrayAdapter<String> presetSpinnerAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, android.R.id.text1, presets);
@@ -441,14 +457,78 @@ public class VideoActivity extends AppCompatActivity implements TaskView.OnTaskC
 
             }
         });
-
     }
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.d(TAG, intent.getStringExtra(CompressService.PROGRESS_UPDATE));
+            if(intent.getBooleanExtra(CompressService.TASK_SUCCESS, false)) {
+                tvTaskStatus.setText("Tasks Completed!");
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+
+            if(tasksStatusStringArray == null) {
+                tvTaskStatus.setText("Processing Video...");
+                if(bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED)
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                tasksStatusStringArray = new ArrayList<>(24);
+                tasksStatusStringArray.add(intent.getStringExtra(CompressService.PROGRESS_UPDATE));
+            }else{
+                if(tasksStatusStringArray.size() == 24) tasksStatusStringArray.remove(0);
+                tasksStatusStringArray.add(intent.getStringExtra(CompressService.PROGRESS_UPDATE));
+            }
+
+            if(tasksStatusAdapter == null){
+                tasksStatusAdapter = new TasksStatusAdapter();
+                tasksStatusAdapter.swapData(tasksStatusStringArray);
+                taskStatusRecycler.setAdapter(tasksStatusAdapter);
+            }else tasksStatusAdapter.swapData(tasksStatusStringArray);
+            taskStatusRecycler.scrollToPosition(tasksStatusStringArray.size() - 1);
         }
     };
+
+    private class TasksStatusAdapter extends RecyclerView.Adapter<TaskStatusViewHolder>  {
+
+        ArrayList<String> stringList;
+        public void swapData(ArrayList<String> stringList){
+            this.stringList = stringList;
+            notifyDataSetChanged();
+        }
+
+        public void saveState(Bundle outState){
+            outState.putStringArrayList("sk", stringList);
+        }
+
+        public void restoreState(Bundle savedInstanceState){
+            if(savedInstanceState.containsKey("sk")) stringList = savedInstanceState.getStringArrayList("sk");
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public TaskStatusViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new TaskStatusViewHolder(
+                    LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false)
+            );
+        }
+
+        @Override
+        public void onBindViewHolder(TaskStatusViewHolder holder, int position) {
+            holder.status.setText(stringList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return stringList == null ? 0 : stringList.size();
+        }
+    };
+
+    private class TaskStatusViewHolder extends RecyclerView.ViewHolder {
+        TextView status;
+        public TaskStatusViewHolder(View itemView){
+            super(itemView);
+            status = (TextView) itemView.findViewById(android.R.id.text1);
+            status.setTextColor(ContextCompat.getColor(itemView.getContext(), R.color.gray));
+        }
+    }
 
 }
